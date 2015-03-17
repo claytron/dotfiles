e9a25d1659f804409678bfc21e9bc806255f8911
Clayton Parker <clayton@sixfeetup.com>
2015-03-17 13:55:45 +0000

    i3pystatus config
    git-svn-id: https://svn.sixfeetup.com/svn/private/claytron/dotfiles/trunk@49512 7abc7aa1-7e52-db11-9331-00508bd72530

---
+++ .i3/status.py
@@ -0,0 +1,23 @@
+#!/home/vagrant/.virtualenvs/i3pystatus/bin/python
+import subprocess
+
+from i3pystatus import Status
+
+status = Status(standalone=True)
+
+# Displays clock like this:
+# Tue 30 Jul 23:59
+status.register("clock", format="%a %-d %b %H:%M")
+
+# Shows the average load of the last minute and the last 5 minutes
+# (the default value for format is used)
+status.register("load")
+
+# Shows disk usage of /
+# Format:
+# 42/128G [86G]
+status.register("disk",
+    path="/",
+    format="{used}/{total}G [{avail}G]",)
+
+status.run()
