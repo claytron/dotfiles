d9556654fef469f485447e92856d716262b0aafd
Clayton Parker <clayton@sixfeetup.com>
2015-03-18 04:12:54 +0000

    make i3bar output clearer and more concise
    git-svn-id: https://svn.sixfeetup.com/svn/private/claytron/dotfiles/trunk@49519 7abc7aa1-7e52-db11-9331-00508bd72530

--- .i3/status.py
+++ .i3/status.py
@@ -1,22 +1,61 @@
 import subprocess
-
 from i3pystatus import Status
+
+# Solarized Colors
+BASE03 = '#002b36'
+BASE02 = '#073642'
+BASE01 = '#586e75'
+BASE00 = '#657b83'
+BASE0 = '#839496'
+BASE1 = '#93a1a1'
+BASE2 = '#eee8d5'
+BASE3 = '#fdf6e3'
+YELLOW = '#b58900'
+ORANGE = '#cb4b16'
+RED = '#dc322f'
+MAGENTA = '#d33682'
+VIOLET = '#6c71c4'
+BLUE = '#268bd2'
+CYAN = '#2aa198'
+GREEN = '#859900'
 
 status = Status(standalone=True)
 
 # Displays clock like this:
 # Tue 30 Jul 23:59
-status.register("clock", format="%a %-d %b %H:%M")
+status.register(
+    'clock',
+    color=BASE0,
+    format=' %a %-d %b %H:%M',
+)
 
-# Shows the average load of the last minute and the last 5 minutes
-# (the default value for format is used)
-status.register("load")
+# Fake iStat Menus
+# CPU
+status.register(
+    'cpu_usage_graph',
+    interval=2,
+    graph_style='braille-fill',
+    graph_width=10,
+    start_color=GREEN,
+    end_color=RED,
+)
+# Memory
+status.register(
+    'mem',
+    round_size=0,
+    color=BASE0,
+    warn_color=ORANGE,
+    alert_color=RED,
+    format=' Mem {percent_used_mem}% '
+)
 
 # Shows disk usage of /
-# Format:
-# 42/128G [86G]
-status.register("disk",
-    path="/",
-    format="{used}/{total}G [{avail}G]",)
+status.register(
+    'disk',
+    path='/',
+    color=BASE0,
+    critical_color=RED,
+    format=' Disk {percentage_used}% ',
+)
 
 status.run()
