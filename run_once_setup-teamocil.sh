50b41d202c98d851cf6ee7cc9c3766b0a0416567
Clayton Parker <me@claytron.com>
2024-12-27 15:43:26 -0500

    checkout teamocil via externals

--- run_once_setup-teamocil.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ ! -d "$HOME/.teamocil" ]; then
-    echo "Set up teamocil configs"
-    echo "There's no I in Teamocil. At least not where you think"
-    git clone git@github.com:claytron/teamocil-config.git ~/.teamocil
-fi
