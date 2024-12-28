ce8d364c285f913b71ea9df35140457ae5fe2336
Clayton Parker <me@claytron.com>
2024-12-27 20:16:30 -0500

    install tpm via externals

--- run_once_after_tmux-setup.sh
+++ run_once_after_tmux-setup.sh
@@ -1,17 +1,7 @@
 #!/usr/bin/env bash
 
-echo "set up tmux"
+echo "set up tmux terminfo"
 
 # Get terminal settings correct for italics
-tic $(chezmoi source-path)/tmux.terminfo
-tic $(chezmoi source-path)/tmux-256color.terminfo
-
-# File for per machine settings
-# TODO: use chezmoi for this part properly
-touch ~/.tmux.local
-
-# Set up TPM for plugins
-if [ ! -d "$HOME/.tmux/plugins" ]; then
-    echo "Install TMUX Plugin Manager"
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-fi
+tic "$(chezmoi source-path)"/tmux.terminfo
+tic "$(chezmoi source-path)"/tmux-256color.terminfo
