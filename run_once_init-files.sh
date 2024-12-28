54142d5e186309fce67a0127c2210cf19bcf7ee4
Clayton Parker <me@claytron.com>
2024-12-27 20:29:22 -0500

    init untracked files

---
+++ run_once_init-files.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+touch_me=(
+    .zsh_history
+    .bash_history
+    .zshextras
+    .zshenv_extras
+    .bash_extras
+    .tmux.local
+)
+
+for new_file in "${touch_me[@]}"; do
+    touch "$new_file"
+done
+
+dirs_to_make=(
+    "$HOME"/bin
+    "$HOME"/.backup
+    "$HOME"/.backup/vim/undo
+    "$HOME"/.backup/vim/undo_vim
+    "$HOME"/.backup/vim/swap
+    "$HOME"/.virtualenvs
+    "$HOME"/.buildout
+    "$HOME"/.buildout/eggs
+    "$HOME"/.buildout/downloads
+    "$HOME"/.buildout/zope
+    "$HOME"/.buildout/extends
+    "$HOME"/work/projects
+)
+
+for directory in "${dirs_to_make[@]}"; do
+    mkdir -p "$directory"
+done
+
+# TODO: maybe with chezmoi?
+chmod 700 "$HOME"/work
--- run_once_setup-work-dir.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -d "$HOME/work" ]; then
-    echo "Set up work projects dir"
-    mkdir -p "$HOME/work/projects"
-    chmod 700 "$HOME/work"
-fi
