150cb14b818c1a18cd316772b8d39301bf69ef90
Clayton Parker <me@claytron.com>
2025-06-06 11:43:51 -0400

    fix path since chezmoi command doesn't work

--- run_once_after_tmux-setup.sh
+++ run_once_after_tmux-setup.sh
@@ -3,5 +3,5 @@
 echo "set up tmux terminfo"
 
 # Get terminal settings correct for italics
-tic "$(chezmoi source-path)"/tmux.terminfo
-tic "$(chezmoi source-path)"/tmux-256color.terminfo
+tic ~/.local/share/chezmoi/tmux.terminfo
+tic ~/.local/share/chezmoi/tmux-256color.terminfo
