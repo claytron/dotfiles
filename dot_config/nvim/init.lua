8b30f3a2252ed93792f514658e8b4bd87cdfbfe1
Clayton Parker <me@claytron.com>
2025-06-18 17:04:28 -0400

    add gitsigns keymap

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1173,6 +1173,54 @@ require('lazy').setup {
     config = function()
       require('gitsigns').setup {
         vim.keymap.set('n', 'tgb', require('gitsigns').blame),
+        on_attach = function(bufnr)
+          local gitsigns = require 'gitsigns'
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Actions
+          map('n', '<leader>hs', gitsigns.stage_hunk, { desc = 'Stage hunk' })
+          map('n', '<leader>hr', gitsigns.reset_hunk, { desc = 'Reset hunk' })
+
+          map('v', '<leader>hs', function()
+            gitsigns.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          end, { desc = 'Stage hunk' })
+
+          map('v', '<leader>hr', function()
+            gitsigns.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
+          end, { desc = 'Reset hunk' })
+
+          map('n', '<leader>hS', gitsigns.stage_buffer, { desc = 'Stage buffer' })
+          map('n', '<leader>hR', gitsigns.reset_buffer, { desc = 'Reset buffer' })
+          map('n', '<leader>hp', gitsigns.preview_hunk, { desc = 'Preview hunk' })
+          map('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = 'Preview hunk inline' })
+
+          map('n', '<leader>hb', function()
+            gitsigns.blame_line { full = true }
+          end, { desc = 'Blame line' })
+
+          map('n', '<leader>hd', gitsigns.diffthis, { desc = 'Diff this' })
+
+          map('n', '<leader>hD', function()
+            gitsigns.diffthis '~'
+          end, { desc = 'Diff this' })
+
+          map('n', '<leader>hQ', function()
+            gitsigns.setqflist 'all'
+          end, { desc = 'Quickfix list all' })
+          map('n', '<leader>hq', gitsigns.setqflist, { desc = 'Quickfix list' })
+
+          -- Toggles
+          map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = 'Line blame' })
+          map('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = 'Toggle word diff' })
+
+          -- Text object
+          map({ 'o', 'x' }, 'ih', gitsigns.select_hunk, { desc = 'Select hunk text object' })
+        end,
       }
     end,
   },
