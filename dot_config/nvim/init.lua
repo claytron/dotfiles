a4f95fa4a89f8bd442002a08b190b83940f358f4
Clayton Parker <me@claytron.com>
2025-06-25 20:06:47 -0400

    surround a block of text when in visual block mode

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1789,7 +1789,9 @@ require('lazy').setup {
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      require('mini.surround').setup {
+        respect_selection_type = true,
+      }
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
