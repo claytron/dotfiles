681671e289aaa97d61e34a9e17428e8b417bf4ed
Clayton Parker <me@claytron.com>
2025-11-06 10:40:29 -0500

    disable avante for now

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1934,96 +1934,96 @@ require('lazy').setup {
 
   -- MCP hub                                                    {{{2
   --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  {
-    'ravitemer/mcphub.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
-    config = function()
-      require('mcphub').setup {
-        extensions = {
-          avante = {
-            make_slash_commands = true, -- make /slash commands from MCP server prompts
-          },
-        },
-      }
-    end,
-  },
+  -- {
+  --   'ravitemer/mcphub.nvim',
+  --   dependencies = {
+  --     'nvim-lua/plenary.nvim',
+  --   },
+  --   build = 'npm install -g mcp-hub@latest', -- Installs `mcp-hub` node binary globally
+  --   config = function()
+  --     require('mcphub').setup {
+  --       extensions = {
+  --         avante = {
+  --           make_slash_commands = true, -- make /slash commands from MCP server prompts
+  --         },
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- Avante                                                     {{{2
   --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  {
-    'yetone/avante.nvim',
-    event = 'VeryLazy',
-    version = false, -- Never set this value to "*"! Never!
-    opts = {},
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-    build = 'make',
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter',
-      'nvim-lua/plenary.nvim',
-      'MunifTanjim/nui.nvim',
-      --- The below dependencies are optional,
-      'nvim-telescope/telescope.nvim',
-      'folke/snacks.nvim',
-      'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
-      {
-        -- support for image pasting
-        'HakonHarnes/img-clip.nvim',
-        event = 'VeryLazy',
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        'MeanderingProgrammer/render-markdown.nvim',
-        opts = {
-          file_types = { 'markdown', 'Avante' },
-        },
-        ft = { 'markdown', 'Avante' },
-      },
-    },
-    config = function()
-      require('avante').setup {
-        -- system_prompt as function ensures LLM always has latest MCP server state
-        -- This is evaluated for every message, even in existing chats
-        system_prompt = function()
-          local hub = require('mcphub').get_hub_instance()
-          return hub and hub:get_active_servers_prompt() or ''
-        end,
-        -- Using function prevents requiring mcphub before it's loaded
-        custom_tools = function()
-          return {
-            require('mcphub.extensions.avante').mcp_tool(),
-          }
-        end,
-        disabled_tools = {
-          'list_files', -- Built-in file operations
-          'search_files',
-          'read_file',
-          'create_file',
-          'rename_file',
-          'delete_file',
-          'create_dir',
-          'rename_dir',
-          'delete_dir',
-          'bash', -- Built-in terminal access
-        },
-      }
-    end,
-  },
+  -- {
+  --   'yetone/avante.nvim',
+  --   event = 'VeryLazy',
+  --   version = false, -- Never set this value to "*"! Never!
+  --   opts = {},
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = 'make',
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --     --- The below dependencies are optional,
+  --     'nvim-telescope/telescope.nvim',
+  --     'folke/snacks.nvim',
+  --     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+  --     {
+  --       -- support for image pasting
+  --       'HakonHarnes/img-clip.nvim',
+  --       event = 'VeryLazy',
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { 'markdown', 'Avante' },
+  --       },
+  --       ft = { 'markdown', 'Avante' },
+  --     },
+  --   },
+  --   config = function()
+  --     require('avante').setup {
+  --       -- system_prompt as function ensures LLM always has latest MCP server state
+  --       -- This is evaluated for every message, even in existing chats
+  --       system_prompt = function()
+  --         local hub = require('mcphub').get_hub_instance()
+  --         return hub and hub:get_active_servers_prompt() or ''
+  --       end,
+  --       -- Using function prevents requiring mcphub before it's loaded
+  --       custom_tools = function()
+  --         return {
+  --           require('mcphub.extensions.avante').mcp_tool(),
+  --         }
+  --       end,
+  --       disabled_tools = {
+  --         'list_files', -- Built-in file operations
+  --         'search_files',
+  --         'read_file',
+  --         'create_file',
+  --         'rename_file',
+  --         'delete_file',
+  --         'create_dir',
+  --         'rename_dir',
+  --         'delete_dir',
+  --         'bash', -- Built-in terminal access
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- Snacks                                                     {{{2
   --~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
