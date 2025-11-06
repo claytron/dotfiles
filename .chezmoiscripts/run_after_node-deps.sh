7729fc46ae6cf6d5b3ac4becce54caf980b4fe5e
Clayton Parker <me@claytron.com>
2025-11-05 19:53:39 -0500

    some mermaids

--- .chezmoiscripts/run_after_node-deps.sh
+++ .chezmoiscripts/run_after_node-deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm install -g neovim clipboard-cli livedown deno @anthropic-ai/claude-code
+npm install -g neovim clipboard-cli livedown deno @anthropic-ai/claude-code @mermaid-js/mermaid-cli
