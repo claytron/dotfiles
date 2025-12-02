abbb4a4ab6e2eeb64b43674c50894e5d5f27b486
Clayton Parker <me@claytron.com>
2025-12-02 15:20:20 -0500

    try out gemini

--- .chezmoiscripts/run_after_node-deps.sh
+++ .chezmoiscripts/run_after_node-deps.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-npm install -g neovim clipboard-cli livedown deno @anthropic-ai/claude-code @mermaid-js/mermaid-cli
+npm install -g neovim clipboard-cli livedown deno @anthropic-ai/claude-code @mermaid-js/mermaid-cli @google/gemini-cli
